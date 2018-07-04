76ad0223b334cab43f3476ad68af5f834d489d64
ShruthikaP <31205170+ShruthikaP@users.noreply.github.com>
2018-07-04 17:28:46 +0530

    Delete prob2.py

--- Week-2/Day1/prob2.py
+++
@@ -1,8 +0,0 @@
-import unittest
-def delete_node(node):
-    ptr = node.next
-    if ptr:
-        node.next = ptr.next
-        node.value = ptr.value
-    else:
-        raise ValueError("Cannot delete the tail node..")
