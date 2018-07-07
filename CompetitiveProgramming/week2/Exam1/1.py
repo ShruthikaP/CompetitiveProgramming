403c9032bb427f6624cbe44ca3425bbcc90aa015
ShruthikaP <31205170+ShruthikaP@users.noreply.github.com>
2018-07-07 15:43:24 +0530

    Add files via upload

--- CompetitiveProgramming/week2/Exam1/1.py
+++ CompetitiveProgramming/week2/Exam1/1.py
@@ -1,1 +1,28 @@
+def anagram(s,t):
+    s=s.lower()
+    t=t.lower()
+    l1=[]
+    l2=[]
+    for i in s:
+        if(i!=' '):
+            l1.append(i)
+    for i in t:
+        if(i!=' '):
+            l2.append(i)
+    l1.sort()
+    l2.sort()	
+    if(l1==l2):
+        print(True)
+    else:
+        print(False)
 
+anagram("anagram","nagaram")
+anagram("rat","car")
+anagram("Keep","Peek")
+anagram("Mother In Law","Hitler Woman")
+anagram("School Master","The Classroom")
+anagram("ASTRONOMERS","NO MORE STARS")
+anagram("Toss","Shot")
+anagram("Debit Card","Bad Credit")
+anagram("SiLeNt CAT","LisTen AcT")
+anagram("Dormitory","Dirty Room")
